b7f06fb73a013d158d40bca006392629ca426691
Kevin <bkevin93@hotmail.com>
2019-10-16 22:32:10 +0100

    init

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include 'lib/php/config.php';
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <title><?php if(isset($_GET['title'])){echo $_GET['title'];}else{echo "Kevin's Blog";}?></title>
+
+
+</head>
+<body>
+
+
+
+
+</body>
+</html>
